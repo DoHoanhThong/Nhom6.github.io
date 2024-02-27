5544d193a2fa9f8a63b69450d6a5f8e75618729d
dohoanthong <116729656+dohoanthong@users.noreply.github.com>
2024-02-27 09:32:19 +0700

    new update

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+function register() {
+    var username = document.getElementById('usernameSignup').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('passwordSignup').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    if (password !== confirmPassword) {
+        alert('Xác nhận mật khẩu không khớp!');
+        return;
+    }
+    alert('Đăng ký thành công!');
+
+}
+
+function signIn() {
+    var username = document.getElementById('usernameLogin').value;
+    var password = document.getElementById('passwordLogin').value;
+    if (username !== "" && password !== "") {
+        alert('Đăng nhập thành công!');
+    } else {
+        alert('Hãy nhập đủ thông tin');
+    }
+}

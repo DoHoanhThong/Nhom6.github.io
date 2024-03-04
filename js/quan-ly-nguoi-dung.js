f4ea8569d0d96cf4ac53e94871227e27966fe28c
dohoanthong <116729656+dohoanthong@users.noreply.github.com>
2024-03-05 00:36:53 +0700

    hihhaa

--- js/quan-ly-nguoi-dung.js
+++ js/quan-ly-nguoi-dung.js
@@ -4,28 +4,30 @@ function myFunction() {
     filter = input.value.toUpperCase();
     table = document.getElementById("products-datatable");
     tr = table.getElementsByTagName("tr");
-  
+
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      td1 = tr[i].getElementsByTagName("td")[1];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        var isFindOut = false;
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                isFindOut = true;
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-      }
-      else{
-      if (td1) {
-        txtValue = td1.textContent || td1.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        if (td1 && !isFindOut) {
+            txtValue = td1.textContent || td1.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-      }
-      }
+        isFindOut = false;
+
     }
-  }
+}
